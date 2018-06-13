b62226a33260d95cce7bf591da25c5cc2ea8a233
Trevor Russell <truss.42@gmail.com>
2018-06-13 16:52:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,11 @@ def oxford_comma(array)
     return array[0]
   elsif array.length === 2
     return "#{array[0]} and #{array[1]}"
-  end
+  else
+    i = 0
+    while i < array.length - 1
+      array[i] = array[i] + ","
+    end
+    array[-1] = "and" + array[-1]
+    
 end
