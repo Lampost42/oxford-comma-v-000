cf7817ca1543bb09e79e5de6aefe278234d753b7
Trevor Russell <truss.42@gmail.com>
2018-06-13 16:41:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length === 1 
-    return array[0].to_s
+    return array[0]
   elsif array.length === 2
-    return 
+    return "#{array[0]} and #{array[1]""
   end
 end
